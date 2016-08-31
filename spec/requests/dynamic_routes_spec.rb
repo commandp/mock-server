3f8912c425f938e6450f95233d5f484864bf572d
Jimmy Kuo <jimmy@gogojimmy.net>
2016-08-31 20:32:19 +0800

    fix(request_spec): 修測試

--- spec/requests/dynamic_routes_spec.rb
+++ spec/requests/dynamic_routes_spec.rb
@@ -2,53 +2,19 @@ require 'rails_helper'
 
 describe 'DynamicRouter', type: :request do
 
-  context 'GET' do
-    let(:req) { create(:api_request, request_method: 'get') }
-    it 'works', focus: true do
-      json = {a: 'b'}.to_json
-      get  "/#{req.project.name.downcase}" + req.request_path
-      expect(ActiveSupport::JSON.decode(response.body)).to eq(req.return_json)
-      expect(response.status).to eq(Rack::Utils::SYMBOL_TO_STATUS_CODE[req.status_code.to_sym])
+  context 'with different http method' do
+
+    %w(GET PUT POST PATCH DELETE).each do |attr|
+
+      it "works with #{attr}" do
+        req = create(:api_request, request_method: attr)
+        send(attr.downcase, "/#{req.project.name.downcase}#{req.request_path}")
+        expect(response.body).to eq(req.return_json)
+        expect(response.status).to eq(Rack::Utils::SYMBOL_TO_STATUS_CODE[req.status_code.to_sym])
+      end
+
     end
+
   end
 
-  context 'PUT' do
-    let(:req) { create(:api_request, request_method: 'put', status_code: 200) }
-    it 'works' do
-      json = {a: 'b'}.to_json
-      put "/#{req.project.name}" + req.request_path
-      expect(ActiveSupport::JSON.decode(response.body)).to eq(req.return_json)
-      expect(response.status).to eq(Rack::Utils::SYMBOL_TO_STATUS_CODE[req.status_code.to_sym])
-    end
-  end
-
-  context 'POST' do
-    let(:req) { create(:api_request, request_method: 'post', status_code: 201) }
-    it 'works' do
-      json = {a: 'b'}.to_json
-      post "/#{req.project.name}" + req.request_path
-      expect(ActiveSupport::JSON.decode(response.body)).to eq(req.return_json)
-      expect(response.status).to eq(Rack::Utils::SYMBOL_TO_STATUS_CODE[req.status_code.to_sym])
-    end
-  end
-
-  context 'PATCH' do
-    let(:req) { create(:api_request, request_method: 'patch', status_code: 200) }
-    it 'works' do
-      json = {a: 'b'}.to_json
-      patch "/#{req.project.name}" + req.request_path
-      expect(ActiveSupport::JSON.decode(response.body)).to eq(req.return_json)
-      expect(response.status).to eq(Rack::Utils::SYMBOL_TO_STATUS_CODE[req.status_code.to_sym])
-    end
-  end
-
-  context 'DELETE' do
-    let(:req) { create(:api_request, request_method: 'delete', status_code: 200) }
-    it 'works' do
-      json = {a: 'b'}.to_json
-      delete "/#{req.project.name}" + req.request_path
-      expect(ActiveSupport::JSON.decode(response.body)).to eq(req.return_json)
-      expect(response.status).to eq(Rack::Utils::SYMBOL_TO_STATUS_CODE[req.status_code.to_sym])
-    end
-  end
 end
