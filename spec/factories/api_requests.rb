b4267d4112b375faef3c2308fe8a50534903159e
angelfan <angelfan1314@163.com>
2016-09-24 23:23:55 +0800

    test(factories): 调整一下, 使创建的request_path不重复

--- spec/factories/api_requests.rb
+++ spec/factories/api_requests.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :api_request do
     request_method 'get'
-    request_path '/some_path'
+    sequence(:request_path) { |n| "/some_path#{n}" }
     status_code :ok
     return_json '{"a": "b"}'
     name '厲害的 API'
