68bebaf422c67f28d293ba6c6d9336015df14a4b
angelfan <angelfan1314@163.com>
2016-09-27 23:03:28 +0800

    style(use): string的写法要被遗弃了

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module MockServer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.middleware.use("PathModifier")
+    require 'path_modifier'
+    config.middleware.use PathModifier
   end
 end
 
